be4b92cbd4061b2ae1fb28defcb2f75a82608185
Prince Ali Khan <princealikhan08@gmail.com>
2019-06-14 02:00:55 +0530

    small fix

--- pages/about.js
+++ pages/about.js
@@ -42,57 +42,58 @@ class AboutPage extends Component {
           <title>Prince Ali Khan | About</title>
         </Head>
         <div className={cx('is-col-xs-12', { 'is-col-sm-6': true, 'is-col-md-8': true, 'is-col-lg-8': true })}>
-          {/* <h3 className={'has-color--white has-font-weight--bold'}>Software Developer,</h3> */}
-          <div id="about" style={{ marginTop: 48}}> 
-            <h1 style={{ marginBottom: 0}} className={'has-color--yellow has-font-weight--bold'}>
-              About 🤴
-            </h1>
-            <div className={'has-color--gray'} style={{fontSize: '16px'}}>
-            I'm an enthusiastic web developer --- currently dealing with backed of zoojoo.be and sometimes play with servers ---surrounded with awesome people who build awesomesauce product - when I am not baking <b>code</b> then I am grabbing the beer with my friend.
+          <div className={classes.container}>
+            <div id="about" style={{ marginTop: 48}}> 
+              <h1 style={{ marginBottom: 0}} className={'has-color--yellow has-font-weight--bold'}>
+                About 🤴
+              </h1>
+              <div className={'has-color--white'} style={{fontSize: '16px'}}>
+              I'm an enthusiastic web developer --- currently dealing with backed of zoojoo.be and sometimes play with servers ---surrounded with awesome people who build awesomesauce product - when I am not baking <b>code</b> then I am grabbing the beer with my friend.
 
 
+              </div>
+              
             </div>
-            
-          </div>
+          </div>  
         </div>
         <div className={cx('is-col-xs-12', { 'is-col-sm-6': true, 'is-col-md-4': true, 'is-col-lg-4': true, [classes.mobileView]: true})}>
-          
-        <div id="skills" style={{ marginTop: 48}}>
-          <h3 style={{ marginBottom: 0}} className={'has-color--white has-font-weight--bold'}>
-            Programming Languages
-          </h3>
-          <Divider style={{ marginTop:8,marginBottom: 12}}/>
-          {
-            siteData.languages.map((language, index) => (
-                <Chip 
-                  key={index}
-                  color="secondary" 
-                  avatar={<Avatar alt={language.technology} src={language.icon} />}
-                  label={language.technology}
-                  className={classes.chip}
-                />
-            ))
-          }
-        </div>
+          <div className={classes.container}>
+            <div id="skills" style={{ marginTop: 48}}>
+              <h3 style={{ marginBottom: 0}} className={'has-color--white has-font-weight--bold'}>
+                Programming Languages
+              </h3>
+              <Divider style={{ marginTop:8,marginBottom: 12}}/>
+              {
+                siteData.languages.map((language, index) => (
+                    <Chip 
+                      key={index}
+                      color="secondary" 
+                      avatar={<Avatar alt={language.technology} src={language.icon} />}
+                      label={language.technology}
+                      className={classes.chip}
+                    />
+                ))
+              }
+            </div>
 
-        <div id="tools" style={{ marginTop: 48}}>
-          <h3 style={{ marginBottom: 0}} className={'has-color--white has-font-weight--bold'}>
-            Tools & Frameworks
-          </h3>
-          <Divider style={{ marginTop:8,marginBottom: 12}}/>
-          {
-            siteData.tools.map((tool, index) => (
-                <Chip 
-                  key={index}
-                  color="secondary" 
-                  avatar={<Avatar alt={tool.technology} src={tool.icon} />}
-                  label={tool.technology}
-                  className={classes.chip}
-                />
-            ))
-          }
-        </div>
-
+            <div id="tools" style={{ marginTop: 48}}>
+              <h3 style={{ marginBottom: 0}} className={'has-color--white has-font-weight--bold'}>
+                Tools & Frameworks
+              </h3>
+              <Divider style={{ marginTop:8,marginBottom: 12}}/>
+              {
+                siteData.tools.map((tool, index) => (
+                    <Chip 
+                      key={index}
+                      color="secondary" 
+                      avatar={<Avatar alt={tool.technology} src={tool.icon} />}
+                      label={tool.technology}
+                      className={classes.chip}
+                    />
+                ))
+              }
+            </div>
+          </div>
         </div>
       </div>
     );
